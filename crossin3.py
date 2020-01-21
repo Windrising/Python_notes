696b77627882a91b55171e6c93cf4ee218fef89c
Windrising <gran1029@foxmail.com>
2020-01-22 00:10:06 +0800

    Create crossin3.py

---
+++ crossin3.py
@@ -0,0 +1,23 @@
+#P1
+name = 'Crossin'
+age = 18
+code = 'Python'
+print("%s is %s, he writes %s."%(name,str(age),code))
+
+#P2
+num1 = '3.3'
+num2 = 2.5
+num1=float(num1)
+num3=num1+num2
+print(num3)
+
+#P3
+print(bool(-123)==True)
+print(bool(0)==False)
+print(bool('abc')==True)
+print(bool('False')==True)
+print(bool('')==False)
+print(bool([])==False)
+print(bool({})==False)
+print(bool([''])==True)   
+print(bool(None)==False)
