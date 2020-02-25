1b9e5398db6283d8418d9b8f5dece478139a1dc5
Windrising <gran1029@foxmail.com>
2020-02-25 17:22:17 +0800

    Create crossin7.py

---
+++ crossin7.py
@@ -0,0 +1,18 @@
+#P1
+lst=[1,2,3]
+print(lst[0])
+lst[1]=4
+lst.append(5)
+print(lst)
+
+#P2
+lst = [365, 'everyday', 0.618, True, 2, 5]
+lst2=lst[1:-1]
+print(lst2)
+
+#P3
+word = 'I am Mr Crossin of Python'
+lst=word.split()
+print(lst)
+word=','.join(lst)
+print(word)
